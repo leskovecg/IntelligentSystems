fcfdb4e9f67e994acc793e25c546a970e099af59
Gasper Leskovec <62363084+leskovecg@users.noreply.github.com>
2022-03-20 11:16:07 +0100

    Add files via upload

---
+++ DT/vaja1.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt 
+
+## 1. NALOGA: PLOT A GRAPH
+x = [i for i in range(10)]
+y = [2*i for i in range(10)]
+
+# labele
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
+
+# izriše črto
+plt.plot(x, y)
+
+# izrišče pike
+plt.scatter(x,y)
+
+
+
+## 2. NALOGA: FEATURES AND LABELS
+# dimension (number of features), instancies (number of rows)
+# Features, attributes, independent variable, input -- > X (upper-case)
+# Label, dependent variable, output -- > y (lower-case)
+
+
+
+# nadzorovano strojno učenje:
+## 3.NALOGA: SAVE AND OPEN A MODEL
+
+
+
